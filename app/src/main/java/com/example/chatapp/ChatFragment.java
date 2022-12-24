c8d3cdf771e1019b645cdb31e928a3f57a198569
Emir <emirsoyalan79@gmail.com>
2022-12-24 23:39:24 +0300

    Update ChatFragment to add dummy chat item data

--- app/src/main/java/com/example/chatapp/ChatFragment.java
+++ app/src/main/java/com/example/chatapp/ChatFragment.java
@@ -10,9 +10,20 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+
+import com.example.chatapp.Adapters.UserAdapter;
+import com.example.chatapp.Models.User;
+import com.example.chatapp.databinding.FragmentChatBinding;
+
+import java.util.ArrayList;
 
 
 public class ChatFragment extends Fragment {
+
+    FragmentChatBinding binding;
+    ArrayList<User> userList = new ArrayList<>();
+
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -23,7 +34,66 @@ public class ChatFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_chat, container, false);
+        binding = FragmentChatBinding.inflate(inflater, container, false);
+
+        UserAdapter adapter = new UserAdapter(userList, getContext());
+        binding.chatRecyclerView.setAdapter(adapter);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getContext());
+        binding.chatRecyclerView.setLayoutManager(layoutManager);
+
+        userList.add(
+                new User(
+                        "1",
+                        "12j3ejkfdh24",
+                        "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg",
+                        "Ahmet Mehmet",
+                        "Selam",
+                        "24.12.2022",
+                        "2"
+                )
+        );
+        /*
+        userList.add(
+                new User(
+                        "1",
+                        "12j3ejkfdh24",
+                        "", // TODO validate
+                        "Ahmet Mehmet",
+                        "Selam",
+                        "24.12.2022"
+                        "0"
+                )
+        );*/
+
+        userList.add(new User(
+                "1",
+                "12j3ejkfdh24",
+                "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg",
+                "Ahmet Mehmet",
+                "Selam",
+                "24.12.2022",
+                "0"
+        ));
+        userList.add(new User(
+                "1",
+                "12j3ejkfdh24",
+                "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg",
+                "Ahmet Mehmet",
+                "Selam",
+                "24.12.2022",
+                "2"
+        ));
+        userList.add(new User(
+            "1",
+            "12j3ejkfdh24",
+            "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg",
+            "Ahmet Mehmet",
+            "Selam",
+            "24.12.2022",
+            "2"
+        ));
+        adapter.notifyDataSetChanged();
+        return binding.getRoot();
     }
 
     @Override
