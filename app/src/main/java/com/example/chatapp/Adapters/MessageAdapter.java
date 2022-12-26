1d5bbd161664da0cb76e5079c1c9b7e248d1133f
Emir <emirsoyalan79@gmail.com>
2022-12-27 01:29:10 +0300

    Add selection logic to MessageAdapter

--- app/src/main/java/com/example/chatapp/Adapters/MessageAdapter.java
+++ app/src/main/java/com/example/chatapp/Adapters/MessageAdapter.java
@@ -1,5 +1,6 @@
 package com.example.chatapp.Adapters;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,21 +15,22 @@ import com.example.chatapp.Models.FirebaseUserInstance;
 import com.example.chatapp.Models.WebServiceMessage;
 import com.example.chatapp.R;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class MessageAdapter extends RecyclerView.Adapter<MessageAdapter.ViewHolder> {
 
     private final List<WebServiceMessage> chatMessages;
     private final Context context;
+    ArrayList<Boolean> selected = new ArrayList<>(Collections.nCopies(1000, false)); // Huge issue
 
     private static final int VIEW_TYPE_MESSAGE_RIGHT = 1;
     private static final int VIEW_TYPE_MESSAGE_LEFT = 2;
-    // private static final int VIEW_TYPE_MESSAGE_DATE = 3;
 
     public MessageAdapter(List<WebServiceMessage> chatMessages, Context context) {
         this.chatMessages = chatMessages;
         this.context = context;
-
     }
 
     @Override
@@ -50,16 +52,23 @@ public class MessageAdapter extends RecyclerView.Adapter<MessageAdapter.ViewHold
         View v = LayoutInflater
                 .from(parent.getContext())
                 .inflate(layout, parent, false);
-        return new ViewHolder(v);
+            return new ViewHolder(v);
     }
 
+    @SuppressLint("ResourceType")
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        if (selected.get(position)) {
+            holder.itemView.setBackgroundColor(context.getResources().getColor(R.color.blue_ultra_light));
+        } else {
+            holder.itemView.setBackgroundColor(context.getResources().getColor(R.color.blue_ultra_dark));
+        }
         WebServiceMessage webServiceMessage = chatMessages.get(position);
         holder.bind(webServiceMessage);
+
     }
 
-     class ViewHolder extends RecyclerView.ViewHolder {
+     class ViewHolder extends RecyclerView.ViewHolder implements View.OnLongClickListener {
         private final TextView messageTextView;
         private final TextView messageDateTextView;
         private final TextView seenInfoTextView;
@@ -69,6 +78,7 @@ public class MessageAdapter extends RecyclerView.Adapter<MessageAdapter.ViewHold
             messageTextView = itemView.findViewById(R.id.message_text);
             messageDateTextView = itemView.findViewById(R.id.message_date);
             seenInfoTextView = itemView.findViewById(R.id.seen_info);
+            itemView.setOnLongClickListener(this);
         }
 
         void bind(WebServiceMessage webServiceMessage) {
@@ -76,23 +86,23 @@ public class MessageAdapter extends RecyclerView.Adapter<MessageAdapter.ViewHold
             messageDateTextView.setText(Helpers.parseMessageDate(webServiceMessage.date));
             seenInfoTextView.setText(Helpers.parseSeenText(webServiceMessage.seen));
         }
+
+         @Override
+         public boolean onLongClick(View view) {
+             selected.set(getAdapterPosition(), !selected.get(getAdapterPosition()));
+             notifyItemChanged(getAdapterPosition());
+             return true;
+         }
     }
     
     @Override
     public int getItemViewType(int position) {
         WebServiceMessage message = chatMessages.get(position);
 
-        // If the current user is the sender of the message
-
         if (String.valueOf(message.fromID).equals(FirebaseUserInstance.getInstance().id)) {
             return VIEW_TYPE_MESSAGE_RIGHT;
         } else {
-            // The current user is not the sender of the message
-            //if (message.getIsDate()) {
-            //    return VIEW_TYPE_MESSAGE_DATE;
-            //} else {
             return VIEW_TYPE_MESSAGE_LEFT;
-            //}
         }
     }
 }
