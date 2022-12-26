c2f14b14d3a201ae93fa68b5c5b90dc9aec124e4
Emir <emirsoyalan79@gmail.com>
2022-12-26 05:04:08 +0300

    Add lastSeen to ChatItem Model

--- app/src/main/java/com/example/chatapp/Models/ChatItem.java
+++ app/src/main/java/com/example/chatapp/Models/ChatItem.java
@@ -1,15 +1,16 @@
 package com.example.chatapp.Models;
 
 public class ChatItem {
-    public String id, uuid, profilePic, name, lastMessage, lastMessageDate, unseenMessages;
+    public String id, uuid, profilePic, name, lastMessage, lastMessageDate, unseenMessages, lastSeen;
 
-    public ChatItem(String id, String uuid, String profile_pic, String name, String last_message, String last_message_date, String unseenMessages) {
+    public ChatItem(String id, String uuid, String profile_pic, String name, String lastMessage, String lastMessageDate, String unseenMessages, String lastSeen) {
         this.id = id;
         this.uuid = uuid;
         this.profilePic = profile_pic;
         this.name = name;
-        this.lastMessage = last_message;
-        this.lastMessageDate = last_message_date;
+        this.lastMessage = lastMessage;
+        this.lastMessageDate = lastMessageDate;
         this.unseenMessages = unseenMessages;
+        this.lastSeen = lastSeen;
     }
 }
