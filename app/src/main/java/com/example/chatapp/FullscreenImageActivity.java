07000b37b29189c3233814473b7fbc6a09f7decc
Emir <emirsoyalan79@gmail.com>
2022-12-29 23:32:20 +0300

    Add story view timer

--- app/src/main/java/com/example/chatapp/FullscreenImageActivity.java
+++ app/src/main/java/com/example/chatapp/FullscreenImageActivity.java
@@ -1,8 +1,10 @@
 package com.example.chatapp;
 
 import android.os.Bundle;
-import android.util.Log;
+import android.os.CountDownTimer;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,7 +12,9 @@ import com.example.chatapp.Classes.WebService;
 
 public class FullscreenImageActivity extends AppCompatActivity {
 
-    private ImageView imageView;
+    private final int TIMER_LENGTH_IN_MILLISECONDS = 8000;
+    private final int TIMER_INTERVAL_IN_MILLISECONDS = 10;
+    CountDownTimer timer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,13 +23,30 @@ public class FullscreenImageActivity extends AppCompatActivity {
 
         String imageUrl = getIntent().getStringExtra("image_url");
         String folder = getIntent().getStringExtra("folder");
-        imageView = findViewById(R.id.fullscreen_image_view);
+        ImageView imageView = findViewById(R.id.fullscreen_image_view);
+        ProgressBar bar = findViewById(R.id.progress_bar);
 
         if (folder.equals("statuses")){
             WebService.putStatus(imageView, imageUrl);
+            bar.setVisibility(View.VISIBLE);
+            bar.setMax(TIMER_LENGTH_IN_MILLISECONDS);
+            timer = new CountDownTimer(TIMER_LENGTH_IN_MILLISECONDS, TIMER_INTERVAL_IN_MILLISECONDS) {
+                @Override
+                public void onTick(long millisUntilFinished) {
+                    bar.setProgress(TIMER_LENGTH_IN_MILLISECONDS - (int) millisUntilFinished);
+                }
+
+                @Override
+                public void onFinish() {
+                    finish();
+                }
+            };
+            timer.start();
+
         }
         if (folder.equals("images")){
             WebService.putImageMessage(imageView, imageUrl);
+            bar.setVisibility(View.GONE);
         }
     }
 
